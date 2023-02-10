caf4c65db47d5af75727fae6c73b70fdc15b0981
koreatlwls <koreatlwls@gmail.com>
2023-02-10 17:11:46 +0900

    feat : Retrofit Call Adapter Factory 구현

---
+++ data/src/main/java/com/example/data/remote/ResponseCallAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.data.remote
+
+import retrofit2.Call
+import retrofit2.CallAdapter
+import java.lang.reflect.Type
+
+class ResponseCallAdapter<T>(
+    private val successType: Type
+) : CallAdapter<T, Call<Result<T>>> {
+
+    override fun responseType(): Type = successType
+
+    override fun adapt(call: Call<T>): Call<Result<T>> = ResponseCall(call)
+
+}
