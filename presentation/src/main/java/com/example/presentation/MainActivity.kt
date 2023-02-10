599098eb2bd39ea700fc5a68c2d52ed14d43fa0a
koreatlwls <koreatlwls@gmail.com>
2023-02-10 14:52:49 +0900

    feat : Bottom Navigation 이동 처리 구현 완료

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -1,11 +1,30 @@
-package com.example.presentation
+package com.example.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
     }
+
+    private fun initBottomNavigation(){
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            findNavController(R.id.nav_host)
+        )
+    }
+
 }
