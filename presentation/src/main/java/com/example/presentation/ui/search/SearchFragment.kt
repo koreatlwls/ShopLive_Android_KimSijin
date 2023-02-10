19b234c3656a49dc4d6a4eae388888947908bfe5
koreatlwls <koreatlwls@gmail.com>
2023-02-10 14:40:18 +0900

    feat : BottomNavigation 연결 용 Fragment 구현

---
+++ presentation/src/main/java/com/example/presentation/ui/search/SearchFragment.kt
@@ -0,0 +1,13 @@
+package com.example.presentation.ui.search
+
+import com.example.presentation.R
+import com.example.presentation.databinding.FragmentSearchBinding
+import com.example.presentation.ui.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun initBinding() {
+
+    }
+
+}
