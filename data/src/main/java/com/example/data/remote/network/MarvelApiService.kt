c3acabf962a2e56aafe205a4c0ae10f0cf96b8de
koreatlwls <koreatlwls@gmail.com>
2023-02-10 17:46:13 +0900

    feat : MarvelApiService 구현

---
+++ data/src/main/java/com/example/data/remote/network/MarvelApiService.kt
@@ -0,0 +1,19 @@
+package com.example.data.remote.network
+
+import com.example.data.remote.model.MarvelResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApiService {
+
+    @GET("/v1/public/characters")
+    suspend fun getMarvelCharacters(
+        @Query("nameStartsWith") nameStartsWith: String,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+        @Query("ts") ts: Int,
+        @Query("apikey") apikey: String,
+        @Query("hash") hash: String
+    ): Result<MarvelResponse>
+
+}
